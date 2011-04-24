8d6582766db3909ea13d1291d00af6a9301c1ca1
Patrick Mylund Nielsen <patrick@patrickmylund.com>
2011-04-24 05:17:44 +0200

    Consolidate util into main script

--- teaman.py
+++ teaman.py
@@ -14,8 +14,6 @@ import sys
 import time
 import datetime
 
-import util
-
 # Linux and Windows only
 play_ready_sound = True
 ready_sound_file = 'ready.wav'
@@ -58,6 +56,35 @@ def countdownToTea(duration):
             pass
         else:
             countdownToTea(duration)
+
+def isLinux():
+    return 'linux' in sys.platform
+
+def isWindows():
+    return sys.platform == 'win32'
+
+def playSound(sound_file):
+    if isWindows():
+        import winsound
+        winsound.PlaySound(sound_file, winsound.SND_FILENAME)
+    elif isLinux():
+        from wave import open as waveOpen
+        from ossaudiodev import open as ossOpen
+        s = waveOpen(sound_file, 'rb')
+        (nc, sw, fr, nf, comptype, compname) = s.getparams( )
+        dsp = ossOpen('/dev/dsp', 'w')
+        try:
+            from ossaudiodev import AFMT_S16_NE
+        except ImportError:
+            if byteorder == "little":
+                AFMT_S16_NE = ossaudiodev.AFMT_S16_LE
+            else:
+                AFMT_S16_NE = ossaudiodev.AFMT_S16_BE
+                dsp.setparameters(AFMT_S16_NE, nc, fr)
+                data = s.readframes(nf)
+                s.close()
+                dsp.write(data)
+                dsp.close()
 
 if __name__ == '__main__':
     main()
