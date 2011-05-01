cf2f599d20879808cde60917211fcc71a91cdd56
Patrick Mylund Nielsen <patrick@patrickmylund.com>
2011-05-01 20:25:10 +0200

    Try aplay (ALSA play) first, then OSS on Linux, and new 16bit PCM WAV instead of MPEG3

--- teaman.py
+++ teaman.py
@@ -14,7 +14,6 @@ import sys
 import time
 import datetime
 
-# Linux and Windows only
 play_ready_sound = True
 ready_sound_file = 'ready.wav'
 
@@ -68,23 +67,30 @@ def playSound(sound_file):
         import winsound
         winsound.PlaySound(sound_file, winsound.SND_FILENAME)
     elif isLinux():
-        from wave import open as waveOpen
-        from ossaudiodev import open as ossOpen
-        s = waveOpen(sound_file, 'rb')
-        (nc, sw, fr, nf, comptype, compname) = s.getparams()
-        dsp = ossOpen('/dev/dsp', 'w')
+        import subprocess
+        import errno
         try:
-            from ossaudiodev import AFMT_S16_NE
-        except ImportError:
-            if byteorder == "little":
-                AFMT_S16_NE = ossaudiodev.AFMT_S16_LE
-            else:
-                AFMT_S16_NE = ossaudiodev.AFMT_S16_BE
-                dsp.setparameters(AFMT_S16_NE, nc, fr)
-                data = s.readframes(nf)
-                s.close()
-                dsp.write(data)
-                dsp.close()
+            FNULL = open('/dev/null', 'w')
+            subprocess.Popen(['aplay', '-q', sound_file], stdin=FNULL, stdout=FNULL, stderr=FNULL)
+        except OSError, e:
+            if e.errno == errno.ENOENT:
+                from wave import open as waveOpen
+                from ossaudiodev import open as ossOpen
+                s = waveOpen(sound_file, 'rb')
+                (nc, sw, fr, nf, comptype, compname) = s.getparams()
+                dsp = ossOpen('/dev/dsp', 'w')
+                try:
+                    from ossaudiodev import AFMT_S16_NE
+                except ImportError:
+                    if byteorder == "little":
+                        AFMT_S16_NE = ossaudiodev.AFMT_S16_LE
+                    else:
+                        AFMT_S16_NE = ossaudiodev.AFMT_S16_BE
+                        dsp.setparameters(AFMT_S16_NE, nc, fr)
+                        data = s.readframes(nf)
+                        s.close()
+                        dsp.write(data)
+                        dsp.close()
 
 if __name__ == '__main__':
     main()
